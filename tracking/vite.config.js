28cc3f4de0c18247e0044a3313669996bad70a8d
Adrian Borella <adriannborella@gmail.com>
2025-11-13 12:37:00 -0300

    Activacion de pwa

--- tracking/vite.config.js
+++ tracking/vite.config.js
@@ -12,70 +12,70 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
-    // VitePWA({
-    //   registerType: 'autoUpdate',
-    //   devOptions: {
-    //     enabled: false
-    //   },
-    //   workbox: {
-    //     globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-    //     runtimeCaching: [
-    //       {
-    //         urlPattern: ({ request }) => request.destination === 'document',
-    //         handler: 'NetworkFirst',
-    //         options: {
-    //           cacheName: 'html-cache',
-    //         },
-    //       },
-    //       {
-    //         urlPattern: ({ request }) =>
-    //           ['style', 'script', 'worker'].includes(request.destination),
-    //         handler: 'StaleWhileRevalidate',
-    //         options: {
-    //           cacheName: 'asset-cache',
-    //         },
-    //       },
-    //       {
-    //         urlPattern: ({ url }) =>
-    //           url.origin === self.location.origin &&
-    //           url.pathname.endsWith('.png'),
-    //         handler: 'CacheFirst',
-    //         options: {
-    //           cacheName: 'image-cache',
-    //           expiration: {
-    //             maxEntries: 10,
-    //           },
-    //         },
-    //       },
-    //     ],
-    //   },
-    //   manifest: {
-    //     name: 'Tracker de Valores',
-    //     short_name: 'Tracker',
-    //     start_url: '/',
-    //     display: 'standalone',
-    //     background_color: '#ffffff',
-    //     theme_color: '#3b82f6',
-    //     icons: [
-    //       {
-    //         src: '/pwa-icon-128.png',
-    //         sizes: '128x128',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: '/pwa-icon-512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: '/pwa-icon-512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //         purpose: 'any maskable',
-    //       },
-    //     ],
-    //   },
-    // }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: false
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === 'document',
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'html-cache',
+            },
+          },
+          {
+            urlPattern: ({ request }) =>
+              ['style', 'script', 'worker'].includes(request.destination),
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'asset-cache',
+            },
+          },
+          {
+            urlPattern: ({ url }) =>
+              url.origin === self.location.origin &&
+              url.pathname.endsWith('.png'),
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'image-cache',
+              expiration: {
+                maxEntries: 10,
+              },
+            },
+          },
+        ],
+      },
+      manifest: {
+        name: 'Tracker de Valores',
+        short_name: 'Tracker',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#3b82f6',
+        icons: [
+          {
+            src: '/pwa-icon-128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
